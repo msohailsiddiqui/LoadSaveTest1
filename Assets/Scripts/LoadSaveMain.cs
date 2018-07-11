1c9d0634a38ada48bc69bd3dbd22f11ae4a06dc2
LAPTOP-A1VS2LSD\Sohail <sohail@quixel.se>
2018-07-11 12:01:56 +0500

    - Updated loading of images to support adding new images dynamically

--- Assets/Scripts/LoadSaveMain.cs
+++ Assets/Scripts/LoadSaveMain.cs
@@ -4,82 +4,85 @@ using UnityEngine;
 
 public class LoadSaveMain : MonoBehaviour
 {
-    public GameObject file1Obj;
-    public GameObject file2Obj;
-    public GameObject file3Obj;
-    public GameObject file4Obj;
-    public GameObject file5Obj;
+    public GameObject quadPrefab;
+    public List<string> filesToLoad;
+    private List<GameObject> quadObjs;
+    private List<Material> quadMaterials;
+    private List<RenderTexture> loadedMaps;
 
-    private Material file1Material;
-    private Material file2Material;
-    private Material file3Material;
-    private Material file4Material;
-    private Material file5Material;
+    private RenderTexture loadedMap;
+    private GameObject singleQuad;
+    private Material singleQuadMat;
 
-    private RenderTexture loadedMap1;
-    private RenderTexture loadedMap2;
-    private RenderTexture loadedMap3;
-    private RenderTexture loadedMap4;
-    private RenderTexture loadedMap5;
+    //private string filePath1 = "D:\\SOS\\Work\\Documents\\test case scans\\clover grass\\QvmG7sfy50yXazkr9awV3g_4K_Diffuse.exr";
+    //private string filePath2 = "D:\\SOS\\Work\\Documents\\test case scans\\Grass_rmnkhgp0\\rmnkhgp_4K_Albedo.exr";
+    //private string filePath3 = "D:\\SOS\\Work\\Documents\\test case scans\\Ground_Forest_sbhit3p0\\sbhit3p_4K_Albedo.exr";
+    //private string filePath4 = "D:\\SOS\\Work\\Documents\\test case scans\\Ground_Grass_pjwfo0\\pjwfo_4K_Albedo.exr";
+    //private string filePath5 = "D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Albedo.exr";
+    //private string filePath6 = "D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Normal.exr";
 
-    private string filePath1 = "D:\\SOS\\Work\\Documents\\test case scans\\clover grass\\QvmG7sfy50yXazkr9awV3g_4K_Diffuse.exr";
-    //private string filePath1 = "D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Albedo.exr";
-    //"D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Normal.exr";
-    //"D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Albedo.exr";
-    private string filePath2 = "D:\\SOS\\Work\\Documents\\test case scans\\Grass_rmnkhgp0\\rmnkhgp_4K_Albedo.exr";
-    //private string filePath2 = "D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Normal.exr";
-    private string filePath3 = "D:\\SOS\\Work\\Documents\\test case scans\\Ground_Forest_sbhit3p0\\sbhit3p_4K_Albedo.exr";
-    private string filePath4 = "D:\\SOS\\Work\\Documents\\test case scans\\Ground_Grass_pjwfo0\\pjwfo_4K_Albedo.exr";
-    private string filePath5 = "D:\\SOS\\Work\\Documents\\test case scans\\Soil_Mulch_sfwnbgba\\sfwnbgba_8K_Albedo.exr";
+    private int mapToLoadIndex;
 
     // Use this for initialization
     void Start()
     {
         FreeImageManager.Instance.Test();
-        if(file1Obj != null)
-        {
-            Renderer temp;
-            temp = file1Obj.GetComponent<Renderer>();
-            if(temp != null)
-            {
-                file1Material = temp.material;
-            }
-            temp = null;
-            if(file2Obj)
-                temp = file2Obj.GetComponent<Renderer>();
-            if (temp != null)
-            {
-                file2Material = temp.material;
-            }
-            temp = null;
-            if (file3Obj)
-                temp = file3Obj.GetComponent<Renderer>();
-            if (temp != null)
-            {
-                file3Material = temp.material;
-            }
-            temp = null;
-            if (file4Obj)
-                temp = file4Obj.GetComponent<Renderer>();
-            if (temp != null)
-            {
-                file4Material = temp.material;
-            }
-            temp = null;
-            if (file5Obj)
-                temp = file5Obj.GetComponent<Renderer>();
-            if (temp != null)
-            {
-                file5Material = temp.material;
-            }
 
+        quadObjs = new List<GameObject>();
+        quadMaterials = new List<Material>();
+        loadedMaps = new List<RenderTexture>();
+
+        int xIncrement = 2;
+        int yIncrement = 0;
+        int index = 0;
+
+        foreach (string file in filesToLoad)
+        {
+            if(!string.IsNullOrEmpty( file))
+            {
+                GameObject tempObj = GameObject.Instantiate(quadPrefab);
+                if(xIncrement > 6)
+                {
+                    xIncrement = 0;
+                    yIncrement+=2;
+                }
+                tempObj.transform.position = new Vector3(tempObj.transform.position.x + xIncrement, tempObj.transform.position.y - yIncrement, tempObj.transform.position.z);
+                xIncrement += 2;
+                tempObj.name = "Map" + index;
+                index++;
+                if(tempObj != null)
+                {
+                    quadObjs.Add(tempObj);
+                    Renderer tempRenderer = tempObj.GetComponent<Renderer>();
+                    if(tempRenderer != null)
+                    {
+                        tempRenderer.material = new Material(Shader.Find("Standard"));
+                        quadMaterials.Add(tempRenderer.material);
+                    }
+                }
+            }
         }
+
+        singleQuad = GameObject.Instantiate(quadPrefab);
+        if(singleQuad != null)
+        {
+            Renderer tempRenderer = singleQuad.GetComponent<Renderer>();
+            if (tempRenderer != null)
+            {
+                singleQuadMat = tempRenderer.material;
+            }
+            
+        }
+
+        mapToLoadIndex = 0;
     }
 
     public void CreateRT8K()
     {
-        loadedMap1 = new RenderTexture(8192, 8192, 0, RenderTextureFormat.ARGBFloat);
-        loadedMap1.Create();
+        // This function was to test if creating a render texture alone contributes to the used heap
+        // Yes it does
+        loadedMap = new RenderTexture(8192, 8192, 0, RenderTextureFormat.ARGBFloat);
+        loadedMap.Create();
     }
    
     public void LoadImage()
@@ -91,75 +94,97 @@ public class LoadSaveMain : MonoBehaviour
 
     public void UnloadImage()
     {
-        if(loadedMap1 != null)
+        if(loadedMap != null )
         {
-            file1Material.mainTexture = null;
-            loadedMap1.Release();
-            Destroy(loadedMap1);
+            if(singleQuadMat != null)
+            {
+                singleQuadMat.mainTexture = null;
+            }
+            loadedMap.Release();
+            Destroy(loadedMap);
+        }
+    }
+
+    public void UnloadAllMaps()
+    {
+        if (loadedMaps.Count > 0)
+        {
+            foreach(RenderTexture rt in loadedMaps)
+            {
+                rt.Release();
+                Destroy(rt);
+            }
+            foreach(Material mat in quadMaterials)
+            {
+                mat.mainTexture = null;
+            }
         }
     }
 
     private void LoadImageFromDisk(string filePath)
     {
-        loadedMap1 = FreeImageManager.Instance.LoadImage(filePath, isLinear: false);
-        if (loadedMap1 != null && file1Material != null)
+        loadedMap = FreeImageManager.Instance.LoadImage(filePath, isLinear: false);
+        if (loadedMap != null && singleQuadMat != null)
         {
-            file1Material.mainTexture = loadedMap1;
+            singleQuadMat.mainTexture = loadedMap;
+        }
+    }
+
+    public void LoadMultipleImagesFromDiskOneAtATime()
+    {
+        if (filesToLoad.Count > 0)
+        {
+            if (loadedMaps.Count >= mapToLoadIndex)
+            {
+                if (loadedMaps.Count > mapToLoadIndex)
+                {
+                    if (loadedMaps[mapToLoadIndex] != null)
+                    {
+                        loadedMaps[mapToLoadIndex].Release();
+                        Destroy(loadedMaps[mapToLoadIndex]);
+                        loadedMaps[mapToLoadIndex] = null;
+                    }
+                    loadedMaps[mapToLoadIndex] = FreeImageManager.Instance.LoadImage(filesToLoad[mapToLoadIndex], isLinear: false);
+                    quadMaterials[mapToLoadIndex].mainTexture = loadedMaps[mapToLoadIndex];
+                }
+                else
+                {
+                    loadedMaps.Add(FreeImageManager.Instance.LoadImage(filesToLoad[mapToLoadIndex], isLinear: false));
+                    quadMaterials[mapToLoadIndex].mainTexture = loadedMaps[mapToLoadIndex];
+                }
+                mapToLoadIndex++;
+            }
+            if (mapToLoadIndex >= filesToLoad.Count)
+            {
+                mapToLoadIndex = 0;
+            }
         }
     }
 
     public void LoadMultipleImagesFromDisk()
     {
-        if (file1Obj != null)
+        int index = 0;
+        foreach (string file in filesToLoad)
         {
-            if(loadedMap1 != null)
+            if (loadedMaps.Count > index)
             {
-                loadedMap1.Release();
-                Destroy(loadedMap1);
-                loadedMap1 = null;
+                if (loadedMaps[index] != null)
+                {
+                    loadedMaps[index].Release();
+                    Destroy(loadedMaps[index]);
+                    loadedMaps[index] = null;
+                }
+                loadedMaps[index] = FreeImageManager.Instance.LoadImage(filesToLoad[index], isLinear: false);
+                quadMaterials[index].mainTexture = loadedMaps[index];
             }
-            loadedMap1 = FreeImageManager.Instance.LoadImage(filePath1, isLinear: false);
-            if (loadedMap1 != null && file1Material != null)
+            else
             {
-                file1Material.mainTexture = loadedMap1;
+                loadedMaps.Add(FreeImageManager.Instance.LoadImage(filesToLoad[index], isLinear: false));
+                quadMaterials[index].mainTexture = loadedMaps[index];
             }
+            index++;
         }
-
-        if (file2Obj != null)
-        {
-            loadedMap2 = FreeImageManager.Instance.LoadImage(filePath2, isLinear: false);
-            if (loadedMap2 != null && file2Material != null)
-            {
-                file2Material.mainTexture = loadedMap2;
-            }
-        }
-
-        if (file3Obj != null)
-        {
-            loadedMap3 = FreeImageManager.Instance.LoadImage(filePath3, isLinear: false);
-            if (loadedMap3 != null && file3Material != null)
-            {
-                file3Material.mainTexture = loadedMap3;
-            }
-        }
-
-        if (file4Obj != null)
-        {
-            loadedMap4 = FreeImageManager.Instance.LoadImage(filePath4, isLinear: false);
-            if (loadedMap4 != null && file4Material != null)
-            {
-                file4Material.mainTexture = loadedMap4;
-            }
-        }
-
-        if (file5Obj != null)
-        {
-            loadedMap5 = FreeImageManager.Instance.LoadImage(filePath5, isLinear: false);
-            if (loadedMap5 != null && file5Material != null)
-            {
-                file5Material.mainTexture = loadedMap5;
-            }
-        }
+        mapToLoadIndex = index;
     }
 
     public void CollectGarbage()
@@ -169,11 +194,7 @@ public class LoadSaveMain : MonoBehaviour
 
     private void OnApplicationQuit()
     {
-        if (loadedMap1 != null)
-        {
-            loadedMap1.Release();
-            Destroy(loadedMap1);
-            file1Material.mainTexture = null;
-        }
+        UnloadImage();
+        UnloadAllMaps();
     }
 }
